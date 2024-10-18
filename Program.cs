b7c3d7eef532f73ce52b9c94bbf92cdfa440159d
Diego Neri <diego@diegoneri.com>
2024-10-18 20:39:36 -0300

    Finaliza calculadora

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+double numero01, numero02, resultado;
+string operacao;
+
+Console.WriteLine("-----------");
+Console.WriteLine("Calculadora");
+Console.WriteLine("-----------\n");
+
+Console.WriteLine("----------------------");
+Console.WriteLine("Operações disponíveis:");
+Console.WriteLine("----------------------");
+Console.WriteLine("A. Soma");
+Console.WriteLine("B. Subtração");
+Console.WriteLine("C. Multiplicação");
+Console.WriteLine("D. Divisão");
+Console.WriteLine("----------------------\n");
+
+Console.Write("Operação: ");
+operacao = Console.ReadLine()!.ToUpper();
+Console.Write("Número 01: ");
+numero01 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Número 02: ");
+numero02 = Convert.ToDouble(Console.ReadLine());
+
+switch(operacao)
+{
+    case "A":
+        resultado = numero01 + numero02;
+        break;
+    case "B":
+        resultado = numero01 - numero02;
+        break;
+    case "C":
+        resultado = numero01 * numero02;
+        break;
+    case "D":
+        resultado = numero01 / numero02;
+        break;
+    default:
+        Console.WriteLine("Operação não suportada!");
+        return;
+}
+
+Console.WriteLine($"Resultado: {resultado:N4}");
+
